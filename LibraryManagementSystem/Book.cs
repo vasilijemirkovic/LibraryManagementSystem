de4c61f3ffb095399dbe4ca7838604f30b233ca6
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-04-16 21:47:28 +0200

    created Book model with properties and INotifyPropertyChange support for notifing changes of the statuses

---
+++ LibraryManagementSystem/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace LibraryManagementSystem
+{
+    internal class Book : INotifyPropertyChanged
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+
+        private bool isBorrowed;
+        public string Status => IsBorrowed ? "NO" : "YES";
+
+
+
+        public Book(int id, string title, string author)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Author = author;
+            isBorrowed = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsBorrowed
+        {
+            get => this.isBorrowed;
+            set
+            {
+                isBorrowed = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsBorrowed)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
+            }
+        }
+
+    }
+}
