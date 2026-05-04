f4b7b05b9ed51a186cd7da85791f9438563b9d0c
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-04 13:29:03 +0200

    PropertyChangedEventHandler implemented. Library renamed to readonly

---
+++ LibraryManagementSystem/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace LibraryManagementSystem
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Library library = new Library();
+        public Book SelectedBook {  get; set; }
+
+        public ObservableCollection<Book> FilteredBooks { get; set; }
+
+        public ObservableCollection<Book> Books { get; set; }
+
+        public MainViewModel()
+        {
+            Books = library.GetBooks();
+            FilteredBooks = new ObservableCollection<Book>(Books);
+        }
+        public ObservableCollection<Book> GetBooks()
+        {
+            return Books;
+        }
+        public void AddBook(string title, string author)
+        {
+            library.addBook(title, author);
+        }
+        public void RemoveBook(int id)
+        {
+            library.removeBook(id);
+        }
+        public void BorrowBook(int id)
+        {
+            library.borrowBook(id);
+        }
+        public void ReturnBook(int id)
+        {
+            library.returnBook(id);
+        }
+
+        public void Search(string search)
+        {
+            if (string.IsNullOrWhiteSpace(search))
+            {
+                FilteredBooks = new ObservableCollection<Book>(Books);
+            }
+            else
+            {
+                var filtered = Books
+                    .Where(b => b.Title.ToLower().Contains(search.ToLower())
+                             || b.Author.ToLower().Contains(search.ToLower()));
+
+                FilteredBooks = new ObservableCollection<Book>(filtered);
+            }
+        }
+
+
+    }
+}
