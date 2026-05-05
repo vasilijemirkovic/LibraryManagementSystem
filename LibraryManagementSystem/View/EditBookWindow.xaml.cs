09a16993019c91cd7a55c1cff8d81e923b28211e
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-05 14:02:14 +0200

    implementing Save_Click and Cancel_Click methods for changing or canceling editing book

---
+++ LibraryManagementSystem/View/EditBookWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace LibraryManagementSystem.View
+{
+    /// <summary>
+    /// Interaction logic for EditBookWindow.xaml
+    /// </summary>
+    public partial class EditBookWindow : Window
+    {
+        public string NewTitle { get; private set; }
+        public string NewAuthor { get; private set; }
+        public EditBookWindow(string currentTitle, string currentAuthor)
+        {
+            InitializeComponent();
+            TitleBox.Text = currentTitle;
+            AuthorBox.Text = currentAuthor;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TitleBox.Text) || string.IsNullOrWhiteSpace(AuthorBox.Text))
+            {
+                MessageBox.Show("Please enter both title and author!");
+                return;
+            }
+
+            NewTitle = TitleBox.Text;
+            NewAuthor = AuthorBox.Text;
+            DialogResult = true;
+            Close();
+        }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+    }
+}
