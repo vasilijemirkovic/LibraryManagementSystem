db04000a995f858b0a7de83c2774074bd1766e27
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-12 13:46:56 +0200

    Database for library created. DbSet that will keep the info created.

---
+++ LibraryManagementSystem/Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Data
+{
+    internal class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
