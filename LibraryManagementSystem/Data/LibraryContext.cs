74baa696ef01a0b0e20c66f2b6257f2be6e9a9df
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-12 13:49:03 +0200

    Method configures that the database in the file library.db will be used.

--- LibraryManagementSystem/Data/LibraryContext.cs
+++ LibraryManagementSystem/Data/LibraryContext.cs
@@ -10,6 +10,9 @@ namespace LibraryManagementSystem.Data
     internal class LibraryContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite("Data Source=library.db");
+        }
 
     }
 }
