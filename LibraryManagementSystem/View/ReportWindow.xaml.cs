1d146abc02b2f1e36f36c4f2c907dd6b212743ec
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-13 13:57:50 +0200

    LoadRaport calculates the percentage of available/borrowed books and writes it in the xaml

--- LibraryManagementSystem/View/ReportWindow.xaml.cs
+++ LibraryManagementSystem/View/ReportWindow.xaml.cs
@@ -28,7 +28,32 @@ namespace LibraryManagementSystem.View
 
         private void LoadReport(ObservableCollection<Book> books)
         {
-            throw new NotImplementedException();
+            int total = books.Count;
+            int borrowedBooks = books.Count(eachBook => eachBook.IsBorrowed);
+            int availableBooks = total - borrowedBooks;
+
+            TotalCount.Text = total.ToString();
+            AvailableCount.Text = availableBooks.ToString();
+            BorrowedCount.Text = borrowedBooks.ToString();
+
+            if (total == 0)
+            {
+                AvailablePercent.Text = "0%";
+                BorrowedPercent.Text = "0%";
+                return;
+            }
+
+            double availableRatio = (double)availableBooks / total;
+            double borrowedRatio = (double)borrowedBooks / total;
+
+            AvailablePercent.Text = $"{(int)(availableRatio * 100)}%";
+            BorrowedPercent.Text = $"{(int)(borrowedRatio * 100)}%";
+
+            AvailableBar.Width = availableRatio * 340;
+            BorrowedBar.Width = borrowedRatio * 340;
+
+
+
         }
     }
 }
