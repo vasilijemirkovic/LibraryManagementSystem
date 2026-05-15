96721304bafe01266f5e7e8d1f8d3e29d9564872
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-15 14:38:49 +0200

    show method implmented. It sets the text to the toast window

--- LibraryManagementSystem/View/ToastNotification.xaml.cs
+++ LibraryManagementSystem/View/ToastNotification.xaml.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace LibraryManagementSystem.View
 {
-    /// <summary>
-    /// Interaction logic for ToastNotification.xaml
-    /// </summary>
     public partial class ToastNotification : UserControl
     {
-        public ToastNotification()
-        {
+        public ToastNotification() {
             InitializeComponent();
+        }
+
+        public void Show(string message, bool success = true)
+        {
+            ToastMessage.Text = message;
+            ToastBorder.Background = success
+                ? new SolidColorBrush(Color.FromRgb(76, 175, 80))
+                : new SolidColorBrush(Color.FromRgb(244, 67, 54));
+
+            var storyboard = (Storyboard)Resources["ShowToast"];
+            storyboard.Begin(this);
         }
     }
 }
