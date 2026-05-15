692646099e04f64bdd293e65c1b3bb9e383d3c2a
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-15 13:48:45 +0200

    async/await added to the library.cs so the UI doesn't have to wait for the database to confirm the changes

--- LibraryManagementSystem/Model/Library.cs
+++ LibraryManagementSystem/Model/Library.cs
@@ -25,31 +25,31 @@ namespace LibraryManagementSystem
             books = new ObservableCollection<Book>(context.Books.ToList());
         }
 
-        public bool addBook(string title, string author) {
+        public async Task<bool> addBook(string title, string author) {
 
             bool duplicate = books.Any(book => book.Title.ToLower() == title.ToLower() && book.Author.ToLower() == author.ToLower());
             if (duplicate) return false;
 
             var bookToAdd = new Book { Title = title, Author = author };
             context.Books.Add(bookToAdd);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
             books.Add(bookToAdd);
             return true;
         }
 
-        public bool removeBook(int Id)
+        public async Task<bool> removeBook(int Id)
         {
             var bookToDelete = books.FirstOrDefault(b => b.Id == Id);
 
             if (bookToDelete == null) return false;
 
             context.Books.Remove(bookToDelete);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
             books.Remove(bookToDelete);
             return true;
         }
 
-        public bool borrowBook(int Id)
+        public async Task<bool> borrowBook(int Id)
         {
             var bookToBorrow = books.FirstOrDefault(b => b.Id == Id);
 
@@ -57,11 +57,11 @@ namespace LibraryManagementSystem
 
             bookToBorrow.IsBorrowed = true;
             bookToBorrow.BorrowedDate = DateTime.Now;
-            context.SaveChanges();
+            await context.SaveChangesAsync();
             return true;
         }
 
-        public bool returnBook(int Id)
+        public async Task<bool> returnBook(int Id)
         {
             var bookToReturn = books.FirstOrDefault(b => b.Id == Id);
 
@@ -70,21 +70,21 @@ namespace LibraryManagementSystem
             bookToReturn.IsBorrowed = false;
             bookToReturn.BorrowedDate = null;
 
-            context.SaveChanges();
+            await context.SaveChangesAsync();
             return true;
         }
         public ObservableCollection<Book> GetBooks() {
             return books;
         }
 
-        public bool EditBook(int id, string newTitle, string newAuthor)
+        public async Task<bool> EditBook(int id, string newTitle, string newAuthor)
         {
             var bookToEdit = books.FirstOrDefault(b => b.Id == id);
             if (bookToEdit == null) return false;
 
             bookToEdit.Title = newTitle;
             bookToEdit.Author = newAuthor;
-            context.SaveChanges();
+            await context.SaveChangesAsync();
             return true;
         }
     }
