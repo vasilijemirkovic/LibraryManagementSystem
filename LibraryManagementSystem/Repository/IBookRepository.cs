ae0b031ba855cc425827a5982aa4a4a4c9fc07b2
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-17 14:20:03 +0200

    Created a new interface for repository layer, which is between model and controller

---
+++ LibraryManagementSystem/Repository/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Repository
+{
+    internal interface IBookRepository
+    {
+    }
+}
