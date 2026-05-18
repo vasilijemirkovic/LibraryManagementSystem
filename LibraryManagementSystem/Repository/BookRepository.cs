4728d7a56ccd3a107d03f9695c1793ef914f928e
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-18 19:30:18 +0200

    Implemented methods body.

--- LibraryManagementSystem/Repository/BookRepository.cs
+++ LibraryManagementSystem/Repository/BookRepository.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Repository
 {
-    internal class BookRepository
+    internal class BookRepository : IBookRepository
     {
+        public Task<bool> Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Borrow(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Edit(int id, string newTitle, string newAuthor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Book> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Return(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
