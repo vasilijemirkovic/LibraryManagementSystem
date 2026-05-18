eda8da3c869dfb3c04d71a5f4735277369b6669b
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-18 19:29:24 +0200

    Instantiated methods in interface.

--- LibraryManagementSystem/Repository/IBookRepository.cs
+++ LibraryManagementSystem/Repository/IBookRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace LibraryManagementSystem.Repository
 {
     internal interface IBookRepository
     {
+        ObservableCollection<Book> GetAll();
+        Task<bool> Add(Book book);
+        Task<bool> Remove(int id);
+        Task<bool> Edit(int id, string newTitle, string newAuthor);
+        Task<bool> Borrow(int id);
+        Task<bool> Return(int id);
     }
 }
