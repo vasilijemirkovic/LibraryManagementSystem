8b5903e1cf6aba43e8d83b4887d7bbf759f9f345
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-25 14:32:00 +0200

    All of the member's properties created. It also implements the INotifyChanged interface for changing in the database.

--- LibraryManagementSystem/Model/Member.cs
+++ LibraryManagementSystem/Model/Member.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Model
 {
-    class Member
+    class Member : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int id;
+        public int Id
+        {
+            get => id;
+            set { id = value; OnPropertyChanged(nameof(Id)); }
+        }
+
+        private string name = "";
+        public string Name
+        {
+            get => name;
+            set { name = value; OnPropertyChanged(nameof(Name)); }
+        }
+
+        private string email = "";
+        public string Email
+        {
+            get => email;
+            set { email = value; OnPropertyChanged(nameof(Email)); }
+        }
+
+        private string phone = "";
+        public string Phone
+        {
+            get => phone;
+            set { phone = value; OnPropertyChanged(nameof(Phone)); }
+        }
+
+        public Member() { }
+
     }
 }
