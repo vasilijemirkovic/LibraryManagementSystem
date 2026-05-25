04b828b2288a24162e23fba39f70dae9127fac34
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-25 14:57:47 +0200

    IMemberRepository interface created.

---
+++ LibraryManagementSystem/Repository/IMemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagementSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Repository
+{
+    interface IMemberRepository
+    {
+        ObservableCollection<Member> GetAll();
+        Task<bool> Add(Member member);
+        Task<bool> Remove(int id);
+        Task<bool> Edit(int id, string name, string email, string phone);
+    }
+}
