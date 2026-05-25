4a87e5c5ca9aaac7ad22e5208222f69493b7ebb1
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-25 19:15:24 +0200

    added support for the members with all of its methods

--- LibraryManagementSystem/Model/Library.cs
+++ LibraryManagementSystem/Model/Library.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Model;
 using LibraryManagementSystem.Repository;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -18,40 +19,63 @@ namespace LibraryManagementSystem
     internal class Library
     {
 
-        private readonly IBookRepository iBookRepository;
+        private readonly IBookRepository bookRepository;
+        private readonly IMemberRepository memberRepository;
 
 
         public Library() {
             var context = new LibraryContext();
-            iBookRepository = new BookRepository(context);
+            bookRepository = new BookRepository(context);
+            memberRepository = new MemberRepository(context);
         }
 
         public async Task<bool> addBook(string title, string author) {
             var book = new Book { Title = title, Author = author };
-            return await iBookRepository.Add(book);
+            return await bookRepository.Add(book);
         }
 
         public async Task<bool> removeBook(int Id)
         {
-            return await iBookRepository.Remove(Id);
+            return await bookRepository.Remove(Id);
         }
 
         public async Task<bool> editBook(int id, string newTitle, string newAuthor)
         {
-            return await iBookRepository.Edit(id, newTitle, newAuthor);
+            return await bookRepository.Edit(id, newTitle, newAuthor);
         }
 
-        public async Task<bool> borrowBook(int Id)
+        public async Task<bool> borrowBook(int Id, int memberId)
         {
-            return await iBookRepository.Borrow(Id);
+            return await bookRepository.Borrow(Id, memberId);
         }
 
         public async Task<bool> returnBook(int Id)
         {
-            return await iBookRepository.Return(Id);
+            return await bookRepository.Return(Id);
         }
         public ObservableCollection<Book> getBooks() {
-            return iBookRepository.GetAll();
+            return bookRepository.GetAll();
+        }
+
+        public async Task<bool> AddMember(string name, string email, string phone)
+        {
+            var member = new Member { Name = name, Email = email, Phone = phone };
+            return await memberRepository.Add(member);
+        }
+
+        public async Task<bool> RemoveMember(int id)
+        {
+            return await memberRepository.Remove(id);
+        }
+
+        public async Task<bool> EditMember(int id, string name, string email, string phone)
+        {
+            return await memberRepository.Edit(id, name, email, phone);
+        }
+
+        public ObservableCollection<Member> GetMembers()
+        {
+            return memberRepository.GetAll();
         }
     }
 }
