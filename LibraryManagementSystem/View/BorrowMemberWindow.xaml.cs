2a69622933ce88337b15a7e10c5c5a678edb7db8
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-26 15:20:34 +0200

    Implement the logic of the borrow mamber window

--- LibraryManagementSystem/View/BorrowMemberWindow.xaml.cs
+++ LibraryManagementSystem/View/BorrowMemberWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LibraryManagementSystem.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,32 @@ namespace LibraryManagementSystem.View
     /// </summary>
     public partial class BorrowMemberWindow : Window
     {
-        public BorrowMemberWindow()
+        public Member? SelectedMember { get; private set; }
+
+        public BorrowMemberWindow(ObservableCollection<Member> members)
         {
             InitializeComponent();
+            MembersGrid.ItemsSource = members;
+        }
+
+        private void Borrow_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedMember = MembersGrid.SelectedItem as Member;
+
+            if (SelectedMember == null)
+            {
+                MessageBox.Show("Select a member first!");
+                return;
+            }
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
         }
     }
 }
