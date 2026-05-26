089a9b3eef3b6c595949316425a6526744d60909
Vasilije Mirkovic <vasilijemirkovic@gmail.com>
2026-05-26 20:04:44 +0200

    Add a logic for Members_Click button

--- LibraryManagementSystem/View/MemberWindow.xaml.cs
+++ LibraryManagementSystem/View/MemberWindow.xaml.cs
@@ -118,7 +118,12 @@ namespace LibraryManagementSystem.View
             if (result == MessageBoxResult.Yes) await mainViewModel.RemoveMember(selectedMember.Id);
 
         }
-
+        private void Members_Click(object sender, RoutedEventArgs e)
+        {
+            var memberWindow = new MemberWindow(mainViewModel);
+            memberWindow.Owner = this;
+            memberWindow.ShowDialog();
+        }
 
     }
 }
